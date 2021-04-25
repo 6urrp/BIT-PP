e392ec243904de7dc5beaca61c810f8c01c0cc07
6urrp <sndrtasic@gmail.com>
2021-04-25 21:21:08 +0200

    Adding new functions

--- functions/functionExercise6.js
+++ functions/functionExercise6.js
@@ -193,3 +193,150 @@ function returnElement (array) {
 console.log(returnElement(emptyArray));
 
 
+
+/*12. Write a function to find the average of N elements. Make the function flexible to receive
+dynamic number or parameters. */
+
+
+function averageElement () {
+    var result = 0;
+    var average;
+
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    average = result / arguments.length; //zasto kad console.logujem imam i undefined pored rezultata?!
+    return average;
+}
+
+var a = 5;
+var b = 9;
+var c = 13;
+var d = 3;
+var f = 21;
+
+console.log(averageElement(a, b, c, d, f));
+
+
+/*13. Write a function to find all the numbers greater than the average. */
+
+function greaterThanAverage () {
+    var result = 0;
+    var average;
+    var greater = [];
+    
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    average = result / arguments.length;
+
+    for (var j = 0; j < arguments.length; j++) {
+        if (arguments[j] > average) {
+            greater[greater.length] = arguments[j];
+        }
+    }
+    return greater;
+}
+
+var a = 5;
+var b = 9;
+var c = 13;
+var d = 3;
+var f = 21;
+
+console.log(greaterThanAverage(a, b, c, d, f));
+
+
+//drugi nacin sa dve funkcije 
+
+
+function averageElement (arguments) { //ZASTO MORAM ARGUMENTE DA PISEM
+    var result = 0;
+    var average = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    average = result / arguments.length; 
+    console.log(average);
+    return average;
+}
+
+console.log(averageElement(2, 4, 5, 6, 7)); //ZASTO MI OVDE DAJE NAN
+
+function greaterThanAverage () {
+    var result = [];
+    var avg = averageElement(arguments); //davace mi prazan niz ukoliko nemam argumente u prvoj
+    console.log(avg);
+    console.log(arguments);
+    for (var i = 0; i < arguments.length; i++) {
+        if(arguments[i] > avg) {
+            result[result.length] = arguments[i];
+        }
+    }
+    return result;
+}
+
+console.log(greaterThanAverage(2, 4, 5, 6, 7));
+
+
+
+/*14. The body mass index (BMI) is the ratio of the weight of a person (in kilograms) to the
+square of the height (in meters). Write a function that takes two parameters, weight and
+height, computes the BMI, and prints the corresponding BMI category:
+● Starvation: less than 15
+● Anorexic: less than 17.5
+● Underweight: less than 18.5
+● Ideal: greater than or equal to 18.5 but less than 25
+● Overweight: greater than or equal to 25 but less than 30
+● Obese: greater than or equal to 30 but less than 40
+● Morbidly obese: greater than or equal to 40*/
+
+function bodyMassIndex (weight, height) {
+    var result;
+    var calculateBMI = weight / (height * height);
+    
+    switch (true) { // ovde ide TRUE, pitamo se da li je uslov true/false, a ne ide calculateBMI!!!
+        case calculateBMI < 15:
+            result = "Starvation: less than 15";
+            break;
+        case calculateBMI < 17.5:
+            result = "Axorexic: less than 17.5";
+            break;
+        case calculateBMI < 18.5:
+            result = "Underweight: less than 18.5";
+            break;
+        case calculateBMI >= 18.5 && calculateBMI < 25:
+            result = "Ideal: greater than or equal to 18.5 but less than 25";
+            break;
+        case calculateBMI >= 25 && calculateBMI < 30:
+            result = "Overweight: greater than or equal to 25 but less than 30";
+            break;
+        case calculateBMI >= 30 && calculateBMI < 40:
+            result = "Obese: greater than or equal to 30 but less than 40";
+            break;
+        case calculateBMI >=40:
+            result = "Morbidly obese: greater than or equal to 40";
+            break;
+    }
+    return result; 
+}
+
+console.log(bodyMassIndex(65, 1.68));
+
+
+
+
+/*15. Write a function that takes a list of strings and prints them, one per line, in a rectangular
+frame.:
+For example the list [&quot;Hello&quot;, &quot;World&quot;, &quot;in&quot;, &quot;a&quot;, &quot;frame&quot;] gets
+printed as:
+*********
+* Hello *
+* World *
+* in    *
+* a     *
+* frame *
+*********        */
+
+
+
