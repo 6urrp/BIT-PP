1918eb4ae38b0fb1d16d48527885f6fdf91d48ef
6urrp <sndrtasic@gmail.com>
2021-06-01 06:18:12 +0200

    airport.js in process

---
+++ 08_objects/OOP-Exercises/airport.js
@@ -0,0 +1,51 @@
+"strict mode";
+
+(function () {
+
+    function Person (name, surname) {
+        this.firstname = name;
+        this.lastname = surname;
+        this.getData 
+    }
+
+    var me = new Person ("Sandra", "Tasic");
+    console.log(me);
+
+    function Seat (number, ctgr) {
+        if (typeof number === "undefined") {
+            this.seatNumber = parseInt(((100 - 10) * Math.random()) + 10);
+        } else if (typeof number === "number"){
+            this.seatNumber = number;
+        }
+
+        if (typeof ctgr === "undefined") {
+            this.category = "e";
+        } else if (ctgr === "e" || ctgr ==="b") {
+            this.category = ctgr;
+        }
+    }
+
+    var st = new Seat ();
+    console.log(st);
+
+    function Passenger (person, seat) {
+        this.person = person;
+        this.seat = seat;
+    }
+
+    var pas = new Passenger(me, st);
+    console.log(pas.person);
+
+    function Flight (rel, date) {
+        this.relation = rel;
+        this.date = date;
+        this.passengers = [];
+    }
+
+    function Airport (flights) {
+        this.name = "Nikola Tesla";
+        this.listOfFlights = flights;
+    }
+
+})();
+
