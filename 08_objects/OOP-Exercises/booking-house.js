5a0a02c50632095b43de6f565ccf33f56f55a264
6urrp <sndrtasic@gmail.com>
2021-06-06 17:03:47 +0200

    Update on booking-house exercise.

--- 08_objects/OOP-Exercises/booking-house.js
+++ 08_objects/OOP-Exercises/booking-house.js
@@ -51,6 +51,13 @@
             throw new Error ("Country field is required!")
         }
         this.country = country;
+        this.getMethod = function () {
+            var abbr = abbrevation(this.country.countryName);
+            var expectedAmount = (this.country.odds * this.betAmount).toFixed(2);
+            var age = getAge(this.person.dateOfBirth);
+            return abbr + ", " + expectedAmount + " eur, " + this.person.name + " " + this.person.surname + ", " + age + " years";
+            
+        }
     }
 
     function Address (country, city, code, street, number) {
@@ -60,45 +67,140 @@
         this.street = street;
         this.streetNumber = number;
         this.getMethod = function () {
-            var abbrevation = this.country[0];
-            var vowels = ["a", "e", "i", "o", "u"];
-            for (var i = 1; i < this.country.length; i++) {
-                if (!(vowels.includes(this.country[i].toLowerCase()))) {
-                    abbrevation += this.country[i];
-                }
-            }
-
-            return this.street + " " + this.streetNumber + ", " + this.postalCode + " " + this.city + ", " + abbrevation.toUpperCase().slice(0, 2); 
+            var country = abbrevation(this.country); 
+            return this.street + " " + this.streetNumber + ", " + this.postalCode + " " + this.city + ", " + country;
         }
     };
 
     function BettingPlace (address) {
+        if (!address || !(address instanceof Address)) {
+            throw new Error ("Please insert your address!");
+        }
         this.address = address;
         this.listOfPlayers = [];
+        this.addPlayer = function (player) {
+            this.listOfPlayers.push(player);
+        }
+        this.getMethod = function () {
+            var allBets = 0;
+            this.listOfPlayers.forEach(function (player) {
+                allBets += player.betAmount;
+            });
+            return this.address.street + ", " + this.address.postalCode + " " + this.address.city + ", sum of all bets: " + allBets + "eur";
+        }
     };
 
-    function BettingHouse (competition, numbers) {
+    function BettingHouse (competition) {
         this.competition = competition;
         this.listOfBettingPlaces = [];
-        this.numbersOfPlayers = numbers;
+        this.numbersOfPlayers = 0;
+        this.addBettingPlaces = function (place) {
+            this.listOfBettingPlaces.push(place);
+            this.numbersOfPlayers += place.listOfPlayers.length;
+        }
+        this.allData = function () {
+            var country = [];
+            var result = this.competition + ", " + this.listOfBettingPlaces.length + " betting places, " + this.numbersOfPlayers + " bets" + "\n";
+            this.listOfBettingPlaces.forEach(function (place) {
+                result += "\t" + place.getMethod() + "\n"; 
+                place.listOfPlayers.forEach(function (player) {
+                    result += "\t\t" + player.getMethod() + "\n";
+                    country.push(player.country.countryName);
+                })
+                
+            });
+            console.log(country);
+            return result;
+
+        }
     };
 
 
+
+    //object factory
+
+    function createPlayer (person, bet, country) {
+        return new Player(person, bet, country);
+    };
+
+    function createBettingPlace(address) {
+        return new BettingPlace(address);
+    };
+
+
+    //utility functions
+
+    function abbrevation (country) {
+        var vowels = ["a", "e", "i", "o", "u"];
+        var abbr = country[0];
+        for (var i = 1; i < country.length; i++) {
+            
+            if (!(vowels.includes(country[i].toLowerCase()))) {
+                abbr += country[i];
+            }
+        }
+        return abbr.toUpperCase().slice(0, 2);
+    }
+
+    function getAge(birthday) {
+        var today = new Date();
+        var birthDate = new Date(birthday);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var month = today.getMonth() - birthDate.getMonth();
+        if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
 
 
 
     //testing
     
     try {
-        var continent = new Continent();
-        var cntr1 = new Country("Italy", 20, continent.EUROPE);
-        var person1 = new Person("Sandra", "Tasic", "Sep 6 2021");
-        var player1 = new Player("Sandra Tasic", 500, cntr1);
-        var address1 = new Address("Italy", "Venice", 21000, "Polgar Andrasa", 2);
-        console.log(address1.getMethod());
+        var Continent = new Continent();
+        //first player
+        var person1 = new Person("El", "Profesor", "Feb 23 1975");
+        var country1 = new Country("Spain", 250, Continent.EUROPE);
+        var player1 = createPlayer(person1, 3500, country1);
+        //second player
+        var person2 = new Person("Alba", "Flores", "Oct 27 1986");
+        var country2 = new Country("Kenya", 468, Continent.AFRICA);
+        var player2 = createPlayer(person2, 5500, country2);
+        //third player 
+        var person3 = new Person("Arturo", "Roman", "Oct 8 1972");
+        var country3 = new Country("Spain", 120, Continent.EUROPE);
+        var player3 = createPlayer(person3, 1128, country3);
+        //forth player
+        var person4 = new Person("Ursula", "Corbero", "Aug 11 1989");
+        var country4 = new Country("Japan", 88, Continent.ASIA);
+        var player4 = createPlayer(person4, 8845, country4);
+
+        //first betting place
+        var address1 = new Address ("Colorado", "Denver", 80014, "Eliot St", 86)
+        var bettingPlace1 = createBettingPlace(address1);
+        //second betting place
+        var address2 = new Address("Germany", "Berlin", 10115, "Kreuzbergstraße", 21);
+        var bettingPlace2 = createBettingPlace(address2);
+
+        //add players to betting places
+        bettingPlace1.addPlayer(player1);
+        bettingPlace1.addPlayer(player2);
+        
+        bettingPlace2.addPlayer(player3);
+        bettingPlace2.addPlayer(player4);
+
+
+        //add betting places 
+        var bettingHouse = new BettingHouse("Football World Cup Winner,");
+        bettingHouse.addBettingPlaces(bettingPlace1);
+        bettingHouse.addBettingPlaces(bettingPlace2);
+
+        
+        console.log(bettingHouse.allData());
+
+        
     }catch (err) {
         console.log(err.message);
     }
 })();
-
-
