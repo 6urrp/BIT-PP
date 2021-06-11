a279a35b4122c0e4318a55633b9e8744c56ae073
6urrp <sndrtasic@gmail.com>
2021-06-11 18:46:09 +0200

    checkoutAndBuy logic improved

--- 08_objects/OOP-Exercises/shopping.js
+++ 08_objects/OOP-Exercises/shopping.js
@@ -4,10 +4,13 @@
 (function shoppingList () {
     
     function Product (name, price, expDate) {
+        if (!name || !price || !expDate) {
+            throw new Error ("All fields are required!")
+        }
         this.productId = parseInt(((99999 - 10000) * Math.random()) + 10000);
         this.productName = name;
         this.productPrice = price.toFixed(2);
-        this.expirationDate = expDate;
+        this.expirationDate = new Date(expDate);
         this.getInfo = function () {
             var pName = this.productName;
             var letter = (pName.charAt(0).concat(pName.charAt(pName.length-1))).toUpperCase();
@@ -20,13 +23,12 @@
     function ShoppingBag () {
         this.listOfProducts = [];
         this.addProduct = function (product) {
-            if (Date.parse(product.expirationDate) > Date.now()) {
+            if (product.expirationDate.getTime() > Date.now()) {
                 this.listOfProducts.push(product);
                 return "Your product has valid expiration date!";
             } else {
                 return "Your product has expired!";
             }
-
         }
         this.averagePrice = function () {
             var average = this.calculateTotalPrice() / this.listOfProducts.length;
@@ -56,20 +58,46 @@
 
     
     function PaymentCard (balance, status, validDate) {
+        if (!balance || !status || !validDate) {
+            throw new Error ("All fields are required!")
+        }
         this.accountBalance = balance.toFixed(2);
         this.status = status;
-        this.valid = validDate;
+        this.valid = new Date (validDate);
     }
     
     function checkoutAndBuy (bag, card) {
+        if (!(bag instanceof ShoppingBag) || !(card instanceof PaymentCard)) {
+            throw new Error ("Invalid input!")
+        }
         var cardAmount = card.accountBalance;
         var totalPrice = bag.calculateTotalPrice();
-        if (cardAmount >= totalPrice) {
-            return "Your purchase is successful!"
+        if (card.valid.getTime() > Date.now()) {
+            if (cardAmount >= totalPrice) {
+                return "Your purchase is successful!"
+            } else {
+                var missingAmount = totalPrice - cardAmount;
+                return "You are missing " + missingAmount.toFixed(2) + " to complete your purchase!"
+            }
         } else {
-            var missingAmount = totalPrice - cardAmount;
-            return "You are missing " + missingAmount.toFixed(2) + " to complete your purchase!"
+            throw new Error ("Your card has expired!");
         }
+        
+        
+    }
+
+    try {
+        var product = new Product ("sir", 250, "Oct 26 2021");
+        var product2 = new Product ("origano", 95, "Dec 22 2023")
+        var card = new PaymentCard(300, true, "Feb 03 2022");
+
+        var bag = new ShoppingBag();
+        bag.addProduct(product);
+        bag.addProduct(product2)
+        var output = checkoutAndBuy(bag, card);
+        console.log(output);
+    } catch (err) {
+        console.log(err.message);
     }
 
 })();
